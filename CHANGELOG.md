42208c1a24a2ec2a0fa2e849fab79503ff1744c7
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-01-08 13:52:43 +0000

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,86 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+The format is based on "Keep a Changelog".  This project adheres to Semantic Versioning.
+
+
+## [2.0.1] - 2018-12-06
+### Fixed
+- process_request:  Changed function parameter mutable argument default to immutable argument default.
+
+
+## [2.0.0] - 2018-05-23
+Breaking Change
+
+### Changed
+- Changed "mongo_libs" calls to new naming schema.
+- Changed "mysql_libs" calls to new naming schema.
+- Changed "cmds_gen" calls to new naming schema.
+- Changed "gen_libs" calls to new naming schema.
+- Changed "arg_parser" calls to new naming schema.
+- Changed function names from uppercase to lowercase.
+- Setup single-source version control.
+
+
+## [1.7.0] - 2018-05-08
+### Changed
+- Changed "server" to "mysql_class" module reference.
+- Changed "commands" to "mysql_libs" module reference.
+- Changed "cmds_mongo" to "mongo_libs" module reference.
+
+### Added
+- Added single-source version control.
+
+
+## [1.6.0] - 2017-08-18
+### Changed
+- Convert program to use local libraries from ./lib directory.
+- Change single quotes to double quotes.
+- Help_Message:  Replace docstring with printing the programs __doc__.
+
+
+## [1.5.0] - 2016-12-20
+### Changed
+- Status:  Changed the way output format is handled in a number of 'if' statements.  Assume standard output unless the -j option is set in args_array.
+- main:  Changed the -A, -C, -D, -S, and -t options so they can accept multiple values or an empty list (all).  Replaced -f option with -j; the default output will be standard out unless -j option is selected for JSON format.
+- Process_Request:  Removed "all" checks from 'if' statements.  Instead an empty list means to process all databases or tables.  Reorganized the checks to use intersetions and differences of sets to determine which databases and tables to process.
+
+
+## [1.4.0] - 2016-12-15
+### Changed
+- Run_Check:  Changed print to use format option.
+- Run_Optimize:  Changed print to use format option.
+- Run_Checksum:  Changed print to use format option.
+- Run_Analyze:  Changed print to use format option.
+
+
+## [1.3.0] - 2016-09-15
+### Changed
+- Run_Analyze:  Changed the way the system databases validated and skipped for the function.
+- Run_Optimize:  Changed the way the system databases validated and skipped for the function.
+- main:  Create list of system databases and pass to other functions.
+
+
+## [1.2.0] - 2016-04-15
+### Changed
+- Run_Program:  Processing for some of the new options and passing to the functions.  Setup a Mongo instance.
+- main:  Added options "-M", "-m", "-f", "-i", and "-o" options to a number of variables and added a number of new function call checks.
+- Optimize:  Pass **kwargs to Process_Request function call.
+- Checksum:  Pass **kwargs to Process_Request function call.
+- Analyze:  Pass **kwargs to Process_Request function call.
+- Check:  Pass **kwargs to Process_Request function call.
+- Process_Request:  Receive **kwargs into function and passed **kwargs to Run_Check, Run_Analyze, Run_Checksum, and Run_Optimize functions.
+
+### Added
+- Status function.
+
+
+## [1.1.0] - 2016-04-04
+### Changed
+- Run_Analyze:  Per MySQL Reference documentation, no system database should be analyzed or optimized.  Removing "mysql", "information_schema", and "performance_schema" from the check.
+- Run_Optimize:  Per MySQL Reference documentation, no system database should be analyzed or optimized.  Removing "mysql", "information_schema", and "performance_schema" from the check.
+
+
+## [1.0.0] - 2016-04-01
+- Initial creation.
+
---
+++ mysql_db_admin.py
@@ -0,0 +1,529 @@
+#!/usr/bin/python
+# Classification (U)
+
+"""Program:  mysql_db_admin.py
+
+    Description:  A MySQL Database Administration program that can run a number
+        of different administration functions such as compacting/defraging a
+        table, checking a table for errors, analyze a table's key distribution
+        (index check), or get a checksum on a table.  The options will allow
+        for for a single object, multiple objects, or all objects.  Also can
+        return the database's status to include uptime, connection usage,
+        memory use, and database server status.
+
+    Usage:
+        mysql_db_admin.py -c file -d path {-C [db_name [db_name ...]]
+            | -A [db_name [db_name ...] | -S [db_name [db_name ...]]
+            | -D [db_name [db_name ...] ] | -M {-j
+            | -i {db_name: table_name} | -m file} | -o dir_path/file}}
+            [ -t [table_name [table_name ...]]] [-v | -h]
+
+    Arguments:
+        -c file => Server configuration file.  Required arg.
+        -d dir path => Directory path to config file (-c). Required arg.
+        -C [database name(s)] => Check a table for errors.
+        -A [database name(s)] => Analyze a table's key distribution
+            (checks the table's indexes).
+        -S [database name(s)] => Return a checksum on a table.
+        -D [database name(s)] => Optimize/defragment a table (command
+            runs an Alter Table and Analyze command on the table).
+        -M Display the current database status, such as uptime, memory
+            use, connection usage, and status.
+        -m file => Mongo config file.  Is loaded as a python, do not
+            include the .py extension with the name.
+        -j => Convert output to JSON format.
+        -i { database:collection } => Name of database and collection.
+            Delimited by colon (:).  Default: sysmon:mysql_db_status
+        -o path/file => Directory path and file name for output.
+        -t [ table name(s) ] =>  Used with the -C, -A, -S & -D options.
+        -v => Display version of this program.
+        -h => Help and usage message.
+
+         NOTE 1:  -v or -h overrides the other options.
+
+        NOTE 2:  Options -A, -C, -D, and -S: If no name is passed, will
+             do all database names, can also pass multiple databases
+            names to the option, must be space delimited.
+
+        NOTE 3:  Option -t: If not name is passed, will
+            do all table names, can also pass multiple table names to
+            the option, must be space delimited.  If no -t option is
+            used, will do all tables in the database.
+
+        NOTE 4:  Default output is in standard output, unless -j option
+            is selected.
+
+    Notes:
+        Database configuration file format (mysql_{host}.py):
+            # Configuration file for {Database Name/Server}
+            user = "root"
+            passwd = "ROOT_PASSWORD"
+            # DO NOT USE 127.0.0.1 for the master/source, use actual IP.
+            host = "IP_ADDRESS"
+            serv_os = "Linux" or "Solaris"
+            name = "HOSTNAME"
+            port = PORT_NUMBER (default of mysql is 3306)
+            cfg_file = "DIRECTORY_PATH/my.cnf"
+            sid = "SERVER_ID"
+            extra_def_file = "DIRECTORY_PATH/myextra.cfg"
+
+        NOTE:  Include the cfg_file even if running remotely as the file will
+            be used in future releases.
+
+        configuration modules -> name is runtime dependent as it can be
+            used to connect to different databases with different names.
+
+        Defaults Extra File format (filename.cfg):
+            [client]
+            password="ROOT_PASSWORD"
+            socket="DIRECTORY_PATH/mysql.sock"
+
+        NOTE:  The socket information can be obtained from the my.cnf
+            file under ~/mysql directory.
+
+    Example:
+        mysql_db_admin.py -c mysql -d config -D test -t users
+
+"""
+
+# Libraries and Global Variables
+
+# Standard
+# For Python 2.6/2.7: Redirection of stdout in a print command.
+from __future__ import print_function
+import sys
+import datetime
+
+# Local
+import lib.arg_parser as arg_parser
+import lib.gen_libs as gen_libs
+import lib.cmds_gen as cmds_gen
+import mysql_lib.mysql_libs as mysql_libs
+import mysql_lib.mysql_class as mysql_class
+import mongo_lib.mongo_libs as mongo_libs
+import version
+
+# Version
+__version__ = version.__version__
+
+
+def help_message():
+
+    """Function:  help_message
+
+    Description:  Displays the program's docstring which is the help and usage
+        message when -h option is selected.
+
+    Arguments:
+
+    """
+
+    print(__doc__)
+
+
+def run_analyze(SERVER, db, tbl, **kwargs):
+
+    """Function:  run_analyze
+
+    Description:  Calls the analyze table command and prints the results.
+
+    Arguments:
+        (input) SERVER -> Server database instance.
+        (input) db -> Database name.
+        (input) tbl -> Table name.
+        (input) **kwargs:
+            sys_dbs -> List of system databases to skip.
+
+    """
+
+    if db not in kwargs.get("sys_dbs", []):
+
+        for x in mysql_libs.analyze_tbl(SERVER, db, tbl):
+            print("DB: {0:20} Table: {1:50}\t".format(db, tbl), end="")
+            gen_libs.prt_msg(x["Msg_type"], x["Msg_text"])
+
+
+def run_checksum(SERVER, db, tbl, **kwargs):
+
+    """Function:  run_checksum
+
+    Description:  Calls the checksum table command and prints the results.
+
+    Arguments:
+        (input) SERVER -> Server database instance.
+        (input) db -> Database name.
+        (input) tbl -> Table name.
+        (input) **kwargs:
+            None
+
+    """
+
+    for x in mysql_libs.checksum(SERVER, db, tbl):
+        print("DB: {0:20} Table: {1:50}\tCheckSum: {2}".format(db, tbl,
+                                                               x["Checksum"]))
+
+
+def run_optimize(SERVER, db, tbl, **kwargs):
+
+    """Function:  run_optimize
+
+    Description:  Calls the optimize table command and print the results.
+
+    Arguments:
+        (input) SERVER -> Server database instance.
+        (input) db -> Database name.
+        (input) tbl -> Table name.
+        (input) **kwargs:
+            sys_dbs -> List of system databases to skip.
+
+    """
+
+    # Skip some system databases.
+    if db not in kwargs.get("sys_dbs", []):
+
+        for x in mysql_libs.optimize_tbl(SERVER, db, tbl):
+
+            if x["Msg_type"] == "note" and x["Msg_text"] == \
+               "Table does not support optimize, doing recreate + \
+analyze instead":
+                pass
+
+            else:
+                print("DB: {0:20} Table: {1:50}\t".format(db, tbl), end="")
+                gen_libs.prt_msg(x["Msg_type"], x["Msg_text"])
+
+
+def run_check(SERVER, db, tbl, **kwargs):
+
+    """Function:  run_check
+
+    Description:  Calls the check table command and print the results.
+
+    Arguments:
+        (input) SERVER -> Server database instance.
+        (input) db -> Database name.
+        (input) tbl -> Table name.
+        (input) **kwargs:
+            None
+
+    """
+
+    for x in mysql_libs.check_tbl(SERVER, db, tbl):
+        print("DB: {0:20} Table: {1:50}\t".format(db, tbl), end="")
+        gen_libs.prt_msg(x["Msg_type"], x["Msg_text"])
+
+
+def detect_dbs(sub_db_list, full_db_list, **kwargs):
+
+    """Function:  detect_dbs
+
+    Description:  Finds which databases in Database list 1 are not present in
+        Database list 2.
+
+    Arguments:
+        (input) sub_db_list -> Subset Database list.
+        (input) full_db_list -> Full Database list.
+        (input) **kwargs:
+            None
+
+    """
+
+    dbs = list(set(sub_db_list) - set(full_db_list))
+
+    if dbs:
+        print("Warning: Database(s) that do not exist: %s." % (dbs))
+
+
+def process_request(SERVER, func_name, db_name=None, tbl_name=None, **kwargs):
+
+    """Function:  process_request
+
+    Description:  Prepares for the type of check based on the arguments passed
+        to the function and then calls the "func_name" function.
+
+    Arguments:
+        (input) SERVER -> Database server instance.
+        (input) func_name -> Name of a function.
+        (input) db_name -> Database name | 'all'
+        (input) tbl_name -> Table name | 'all' | None.
+        (input) **kwargs:
+            sys_dbs -> List of system databases.
+            multi_val -> List of options that may have multiple values.
+
+    """
+
+    if db_name is None:
+        db_name = []
+
+    if tbl_name is None:
+        tbl_name = []
+
+    db_list = gen_libs.dict_2_list(mysql_libs.fetch_db_dict(SERVER),
+                                   "Database")
+
+    # Process all tables in all databases.
+    if not db_name:
+
+        for db in db_list:
+
+            for tbl in gen_libs.dict_2_list(mysql_libs.fetch_tbl_dict(SERVER,
+                                                                      db),
+                                            "table_name"):
+
+                func_name(SERVER, db, tbl, **kwargs)
+
+    # Process all tables in listed databases.
+    elif not tbl_name:
+
+        detect_dbs(db_name, db_list, **kwargs)
+
+        for db in set(db_name) & set(db_list):
+
+            for tbl in gen_libs.dict_2_list(mysql_libs.fetch_tbl_dict(SERVER,
+                                                                      db),
+                                            "table_name"):
+
+                func_name(SERVER, db, tbl, **kwargs)
+
+    # Process listed tables in listed databases.
+    else:
+
+        detect_dbs(db_name, db_list, **kwargs)
+
+        for db in set(db_name) & set(db_list):
+            tbl_list = gen_libs.dict_2_list(mysql_libs.fetch_tbl_dict(SERVER,
+                                                                      db),
+                                            "table_name")
+
+            tbls = list(set(tbl_name) - set(tbl_list))
+
+            if tbls:
+                print("Warning: Database (%s) Tables that do not exist %s."
+                      % (db, tbls))
+
+            for tbl in set(tbl_name) & set(tbl_list):
+                func_name(SERVER, db, tbl, **kwargs)
+
+
+def analyze(SERVER, args_array, **kwargs):
+
+    """Function:  analyze
+
+    Description:  Sets up the processing for the analyze table command.
+
+    Arguments:
+        (input) SERVER -> Database server instance.
+        (input) args_array -> Array of command line options and values.
+        (input) **kwargs:
+            sys_dbs -> List of system databases to skip.
+            multi_val -> List of options that may have multiple values.
+
+    """
+
+    process_request(SERVER, run_analyze, args_array["-A"],
+                    args_array.get("-t", None), **kwargs)
+
+
+def checksum(SERVER, args_array, **kwargs):
+
+    """Function:  checksum
+
+    Description:  Sets up the processing for the checksum table command.
+
+    Arguments:
+        (input) SERVER -> Database server instance.
+        (input) args_array -> Array of command line options and values.
+        (input) **kwargs:
+            multi_val -> List of options that may have multiple values.
+
+    """
+
+    process_request(SERVER, run_checksum, args_array["-S"],
+                    args_array.get("-t", None), **kwargs)
+
+
+def optimize(SERVER, args_array, **kwargs):
+
+    """Function:  optimize
+
+    Description:  Sets up the processing for the optimization table command.
+
+    Arguments:
+        (input) SERVER -> Database server instance.
+        (input) args_array -> Array of command line options and values.
+        (input) **kwargs:
+            sys_dbs -> List of system databases.
+            multi_val -> List of options that may have multiple values.
+
+    """
+
+    process_request(SERVER, run_optimize, args_array["-D"],
+                    args_array.get("-t", None), **kwargs)
+
+
+def check(SERVER, args_array, **kwargs):
+
+    """Function:  check
+
+    Description:  Sets up the processing for the check table command.
+
+    Arguments:
+        (input) SERVER -> Database server instance.
+        (input) args_array -> Array of command line options and values.
+        (input) **kwargs:
+            multi_val -> List of options that may have multiple values.
+
+    """
+
+    process_request(SERVER, run_check, args_array["-C"],
+                    args_array.get("-t", None), **kwargs)
+
+
+def status(SERVER, args_array, **kwargs):
+
+    """Function:  status
+
+    Description:  Retrieves a number of database status variables and sends
+        them out either in standard out (print) or to a JSON format which
+        is printed and poissibly inserted into a Mongo database.
+
+    Arguments:
+        (input) SERVER -> Database server instance.
+        (input) args_array -> Array of command line options and values.
+        (input) **kwargs:
+            ofile -> file name - Name of output file.
+            db_tbl database:table_name -> Mongo database and table name.
+            class_cfg -> Mongo server configuration.
+
+    """
+
+    SERVER.upd_srv_stat()
+
+    if "-j" in args_array:
+        outdata = {"Application": "MySQL Database",
+                   "Server": SERVER.name,
+                   "Asof": datetime.datetime.strftime(datetime.datetime.now(),
+                                                      "%Y-%m-%d %H:%M:%S")}
+
+    else:
+        print("\nDatabase Status Check for Server: %s" % (SERVER.name))
+
+    if "-j" in args_array:
+        outdata.update({"Memory": {"Current_Usage": SERVER.cur_mem_mb,
+                                   "Max_Usage": SERVER.max_mem_mb,
+                                   "Percent_Used": SERVER.prct_mem},
+                        "Uptime": SERVER.days_up,
+                        "Connections": {"Current_Connected": SERVER.cur_conn,
+                                        "Max_Connections": SERVER.max_conn,
+                                        "Percent_Used": SERVER.prct_conn}})
+
+    else:
+        gen_libs.prt_msg("Uptime (days)", SERVER.days_up, 0)
+        gen_libs.prt_msg("Memory", "", 0)
+        gen_libs.prt_msg("Max Mem", SERVER.max_mem_mb, 1)
+        gen_libs.prt_msg("Current Mem", SERVER.cur_mem_mb, 1)
+        gen_libs.prt_msg("Percent Used", SERVER.prct_mem, 1)
+        gen_libs.prt_msg("Connections", "", 0)
+        gen_libs.prt_msg("Max Connections", SERVER.max_conn, 1)
+        gen_libs.prt_msg("Current Connections", SERVER.cur_conn, 1)
+        gen_libs.prt_msg("Percent Used", SERVER.prct_conn, 1)
+
+    if "-j" in args_array:
+        mongo_libs.json_prt_ins_2_db(outdata, **kwargs)
+
+
+def run_program(args_array, func_dict, **kwargs):
+
+    """Function:  run_program
+
+    Description:  Creates class instance(s) and controls flow of the program.
+
+    Arguments:
+        (input) args_array -> Array of command line options and values.
+        (input) func_dict -> Dictionary list of functions and options.
+        (input) **kwargs:
+            sys_dbs -> List of system databases.
+            multi_val -> List of options that may have multiple values.
+
+    """
+
+    SERVER = mysql_libs.create_instance(args_array["-c"], args_array["-d"],
+                                        mysql_class.Server)
+    SERVER.connect()
+
+    outfile = args_array.get("-o", None)
+    db_tbl = args_array.get("-i", None)
+
+    MONGO_SVR = None
+    if args_array.get("-m", None):
+        MONGO_SVR = gen_libs.load_module(args_array["-m"], args_array["-d"])
+
+    # Intersect args_array and func_dict to determine which functions to call.
+    for x in set(args_array.keys()) & set(func_dict.keys()):
+        func_dict[x](SERVER, args_array, ofile=outfile, db_tbl=db_tbl,
+                     class_cfg=MONGO_SVR, **kwargs)
+
+    cmds_gen.disconnect([SERVER])
+
+
+def main():
+
+    """Function:  main
+
+    Description:  Initializes program-wide used variables and processes command
+        line arguments and values.
+
+    Variables:
+        dir_chk_list -> contains options which will be directories.
+        file_chk_list -> contains the options which will have files included.
+        file_crt_list -> contains options which require files to be created.
+        func_dict -> dictionary list for the function calls or other options.
+        opt_con_req_list -> contains the options that require other options.
+        opt_def_dict -> contains options with their default values.
+        opt_multi_list -> contains the options that will have multiple values.
+        opt_req_list -> contains the options that are required for the program.
+        opt_val_list -> contains options which require values.
+        opt_xor_dict -> contains options which are XOR with its values.
+        sys_dbs -> contains a list of system databases that will be skipped
+            over for some functions.
+
+    Arguments:
+        (input) argv -> Arguments from the command line.
+
+    """
+
+    dir_chk_list = ["-d"]
+    file_chk_list = ["-o"]
+    file_crt_list = ["-o"]
+    func_dict = {"-A": analyze, "-C": check, "-D": optimize, "-S": checksum,
+                 "-M": status}
+    opt_con_req_list = {"-i": ["-m"]}
+    opt_def_dict = {"-t": None, "-A": [], "-C": [], "-D": [], "-S": [],
+                    "-i": "sysmon:mysql_db_status"}
+    opt_multi_list = ["-A", "-C", "-D", "-S", "-t"]
+    opt_req_list = ["-c", "-d"]
+    opt_val_list = ["-c", "-d", "-t", "-A", "-C", "-D", "-S", "-i", "-m", "-o"]
+    opt_xor_dict = {"-A": ["-C", "-D", "-M", "-S"],
+                    "-C": ["-A", "-D", "-M", "-S"],
+                    "-D": ["-A", "-C", "-M", "-S"],
+                    "-S": ["-A", "-C", "-D", "-M"],
+                    "-M": ["-A", "-C", "-D", "-S"]}
+    sys_dbs = ["performance_schema", "information_schema", "mysql"]
+
+    # Process argument list from command line.
+    args_array = arg_parser.arg_parse2(sys.argv, opt_val_list, opt_def_dict,
+                                       multi_val=opt_multi_list)
+
+    if not gen_libs.help_func(args_array, __version__, help_message):
+        if not arg_parser.arg_require(args_array, opt_req_list) \
+           and arg_parser.arg_xor_dict(args_array, opt_xor_dict) \
+           and arg_parser.arg_cond_req(args_array, opt_con_req_list) \
+           and not arg_parser.arg_dir_chk_crt(args_array, dir_chk_list) \
+           and not arg_parser.arg_file_chk(args_array, file_chk_list,
+                                           file_crt_list):
+            run_program(args_array, func_dict, sys_dbs=sys_dbs,
+                        multi_val=opt_multi_list)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
