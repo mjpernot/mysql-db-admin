8c3a7a53883361539a893751c232149139933393
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-07-15 11:48:27 +0000

    Add check.py file

---
+++ test/unit/mysql_db_admin/check.py
@@ -0,0 +1,114 @@
+#!/usr/bin/python
+# Classification (U)
+
+"""Program:  check.py
+
+    Description:  Unit testing of check in mysql_db_admin.py.
+
+    Usage:
+        test/unit/mysql_db_admin/check.py
+
+    Arguments:
+
+"""
+
+# Libraries and Global Variables
+
+# Standard
+import sys
+import os
+
+if sys.version_info < (2, 7):
+    import unittest2 as unittest
+else:
+    import unittest
+
+# Third-party
+import mock
+
+# Local
+sys.path.append(os.getcwd())
+import mysql_db_admin
+import lib.gen_libs as gen_libs
+import version
+
+__version__ = version.__version__
+
+
+class Server(object):
+
+    """Class:  Server
+
+    Description:  Class stub holder for mysql_class.Server class.
+
+    Super-Class:
+
+    Sub-Classes:
+
+    Methods:
+        __init__ -> Class initialization.
+
+    """
+
+    def __init__(self):
+
+        """Method:  __init__
+
+        Description:  Class initialization.
+
+        Arguments:
+
+        """
+
+        pass
+
+
+class UnitTest(unittest.TestCase):
+
+    """Class:  UnitTest
+
+    Description:  Class which is a representation of a unit testing.
+
+    Super-Class:  unittest.TestCase
+
+    Sub-Classes:
+
+    Methods:
+        setUp -> Initialize testing environment.
+        test_check -> Test check function.
+
+    """
+
+    def setUp(self):
+
+        """Function:  setUp
+
+        Description:  Initialization for unit testing.
+
+        Arguments:
+
+        """
+
+        self.server = Server()
+        self.run_check = True
+        self.args_array = {"-C": True}
+
+    @mock.patch("mysql_db_admin.process_request")
+    def test_check(self, mock_process):
+
+        """Function:  test_check
+
+        Description:  Test check function.
+
+        Arguments:
+
+        """
+
+        mock_process.return_value = True
+
+        self.assertFalse(mysql_db_admin.check(self.server, self.run_check,
+                                              self.args_array))
+
+
+if __name__ == "__main__":
+    unittest.main()
