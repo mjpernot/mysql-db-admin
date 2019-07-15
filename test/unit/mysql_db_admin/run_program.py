d5820967a3a83c457b852cf6ce1565f5525014de
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-07-15 18:09:14 +0100

    Add unit test files

---
+++ test/unit/mysql_db_admin/run_program.py
@@ -0,0 +1,149 @@
+#!/usr/bin/python
+# Classification (U)
+
+"""Program:  run_program.py
+
+    Description:  Unit testing of run_program in mysql_db_admin.py.
+
+    Usage:
+        test/unit/mysql_db_admin/run_program.py
+
+    Arguments:
+
+"""
+
+# Libraries and Global Variables
+
+# Standard
+import sys
+import os
+
+if sys.version_info < (2, 7):
+    import unittest2 as unittest
+else:
+    import unittest
+
+# Third-party
+import mock
+
+# Local
+sys.path.append(os.getcwd())
+import mysql_db_admin
+import lib.gen_libs as gen_libs
+import version
+
+__version__ = version.__version__
+
+
+def check(server, args_array, **kwargs):
+
+    """Method:  check
+
+    Description:  Function stub holder for mysql_db_admin.check.
+
+    Arguments:
+        (input) server -> Server instance.
+        (input) args_array -> Stub holder
+        (input) **kwargs
+            ofile -> Stub holder
+            db_tbl -> Stub holder
+            class_cfg -> Stub holder
+
+    """
+
+    return True
+
+
+class Server(object):
+
+    """Class:  Server
+
+    Description:  Class stub holder for mysql_class.Server class.
+
+    Super-Class:
+
+    Sub-Classes:
+
+    Methods:
+        __init__ -> Class initialization.
+
+    """
+
+    def __init__(self):
+
+        """Method:  __init__
+
+        Description:  Class initialization.
+
+        Arguments:
+
+        """
+
+        pass
+
+    def connect(self):
+
+        """Method:  connect
+
+        Description:  Stub method holder for mysql_class.Server.connect.
+
+        Arguments:
+
+        """
+
+        pass
+
+
+class UnitTest(unittest.TestCase):
+
+    """Class:  UnitTest
+
+    Description:  Class which is a representation of a unit testing.
+
+    Super-Class:  unittest.TestCase
+
+    Sub-Classes:
+
+    Methods:
+        setUp -> Initialize testing environment.
+        test_run_program -> Test run_program function.
+
+    """
+
+    def setUp(self):
+
+        """Function:  setUp
+
+        Description:  Initialization for unit testing.
+
+        Arguments:
+
+        """
+
+        self.server = Server()
+        self.args_array = {"-m": True, "-d": True, "-c": True, "-C": True}
+        self.func_dict = {"-C": check}
+
+    @mock.patch("mysql_db_admin.cmds_gen.disconnect")
+    @mock.patch("mysql_db_admin.gen_libs.load_module")
+    @mock.patch("mysql_db_admin.mysql_libs.create_instance")
+    def test_run_program(self, mock_inst, mock_mongo, mock_disconn):
+
+        """Function:  test_run_program
+
+        Description:  Test run_program function.
+
+        Arguments:
+
+        """
+
+        mock_inst.return_value = self.server
+        mock_mongo.return_value = True
+        mock_disconn.return_value = True
+
+        self.assertFalse(mysql_db_admin.run_program(self.args_array,
+                                                    self.func_dict))
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ test/unit/mysql_db_admin/status.py
@@ -0,0 +1,147 @@
+#!/usr/bin/python
+# Classification (U)
+
+"""Program:  status.py
+
+    Description:  Unit testing of status in mysql_db_admin.py.
+
+    Usage:
+        test/unit/mysql_db_admin/status.py
+
+    Arguments:
+
+"""
+
+# Libraries and Global Variables
+
+# Standard
+import sys
+import os
+
+if sys.version_info < (2, 7):
+    import unittest2 as unittest
+else:
+    import unittest
+
+# Third-party
+import mock
+
+# Local
+sys.path.append(os.getcwd())
+import mysql_db_admin
+import lib.gen_libs as gen_libs
+import version
+
+__version__ = version.__version__
+
+
+class Server(object):
+
+    """Class:  Server
+
+    Description:  Class stub holder for mysql_class.Server class.
+
+    Super-Class:
+
+    Sub-Classes:
+
+    Methods:
+        __init__ -> Class initialization.
+
+    """
+
+    def __init__(self):
+
+        """Method:  __init__
+
+        Description:  Class initialization.
+
+        Arguments:
+
+        """
+
+        self.name = "ServerName"
+        self.cur_mem_mb = "cur_mem_mb"
+        self.max_mem_mb = "max_mem_mb"
+        self.prct_mem = "prct_mem"
+        self.days_up = "days_up"
+        self.cur_conn = "cur_conn"
+        self.max_conn = "max_conn"
+        self.prct_conn = "prct_conn"
+
+    def upd_srv_stat(self):
+
+        """Method:  upd_srv_stat
+
+        Description:  Stub method holder for mysql_class.Server.upd_srv_stat.
+
+        Arguments:
+
+        """
+
+        pass
+
+
+class UnitTest(unittest.TestCase):
+
+    """Class:  UnitTest
+
+    Description:  Class which is a representation of a unit testing.
+
+    Super-Class:  unittest.TestCase
+
+    Sub-Classes:
+
+    Methods:
+        setUp -> Initialize testing environment.
+        test_non_json -> Test with in non-JSON format.
+        test_json -> Test with in JSON format.
+
+    """
+
+    def setUp(self):
+
+        """Function:  setUp
+
+        Description:  Initialization for unit testing.
+
+        Arguments:
+
+        """
+
+        self.server = Server()
+        self.args_array = {"-j": True}
+        self.args_array2 = {}
+
+    def test_non_json(self):
+
+        """Function:  test_non_json
+
+        Description:  Test with in non-JSON format.
+
+        Arguments:
+
+        """
+
+        with gen_libs.no_std_out():
+            self.assertFalse(mysql_db_admin.status(self.server,
+                                                   self.args_array2))
+
+    @mock.patch("mysql_db_admin.mongo_libs.json_prt_ins_2_db")
+    def test_json(self, mock_mongo):
+
+        """Function:  test_json
+
+        Description:  Test with in JSON format.
+
+        Arguments:
+
+        """
+
+        mock_mongo.return_value = True
+
+        self.assertFalse(mysql_db_admin.status(self.server, self.args_array))
+
+
+if __name__ == "__main__":
+    unittest.main()
