bc97b0351914af5edd84f95ae38340000a7e5518
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-12-06 12:28:58 +0000

    Update run_program.py v3.1.0

--- test/unit/mysql_db_admin/run_program.py
+++ test/unit/mysql_db_admin/run_program.py
@@ -60,10 +60,6 @@ class Server(object):
 
     Description:  Class stub holder for mysql_class.Server class.
 
-    Super-Class:
-
-    Sub-Classes:
-
     Methods:
         __init__ -> Class initialization.
 
@@ -99,10 +95,6 @@ class UnitTest(unittest.TestCase):
     """Class:  UnitTest
 
     Description:  Class which is a representation of a unit testing.
-
-    Super-Class:  unittest.TestCase
-
-    Sub-Classes:
 
     Methods:
         setUp -> Initialize testing environment.
