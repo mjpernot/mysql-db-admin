2e068197a550c1d940e92eb6181cf31bfa59664a
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-05-14 10:26:38 +0100

    Update test/unit/mysql_db_admin/main.py v3.1.1

--- test/unit/mysql_db_admin/main.py
+++ test/unit/mysql_db_admin/main.py
@@ -35,6 +35,33 @@ import version
 __version__ = version.__version__
 
 
+class ProgramLock(object):
+
+    """Class:  ProgramLock
+
+    Description:  Class stub holder for gen_class.ProgramLock class.
+
+    Methods:
+        __init__ -> Class initialization.
+
+    """
+
+    def __init__(self, cmdline, flavor):
+
+        """Method:  __init__
+
+        Description:  Class initialization.
+
+        Arguments:
+            (input) cmdline -> Argv command line.
+            (input) flavor -> Lock flavor ID.
+
+        """
+
+        self.cmdline = cmdline
+        self.flavor = flavor
+
+
 class UnitTest(unittest.TestCase):
 
     """Class:  UnitTest
@@ -43,8 +70,6 @@ class UnitTest(unittest.TestCase):
 
     Methods:
         setUp -> Initialize testing environment.
-        test_programlock_false -> Test with ProgramLock returns False.
-        test_programlock_true -> Test with ProgramLock returns True.
         test_help_true -> Test help if returns true.
         test_help_false -> Test help if returns false.
         test_arg_req_true -> Test arg_require if returns true.
@@ -57,6 +82,10 @@ class UnitTest(unittest.TestCase):
         test_arg_dir_false -> Test arg_dir_chk_crt if returns false.
         test_arg_file_true -> Test arg_file_chk if returns true.
         test_arg_file_false -> Test arg_file_chk if returns false.
+        test_run_program -> Test run_program function.
+        test_programlock_true -> Test with ProgramLock returns True.
+        test_programlock_false -> Test with ProgramLock returns False.
+        test_programlock_id -> Test ProgramLock with flavor ID.
 
     """
 
@@ -71,59 +100,8 @@ class UnitTest(unittest.TestCase):
         """
 
         self.args_array = {"-c": "CfgFile", "-d": "CfgDir"}
-
-    @mock.patch(
-        "mysql_db_admin.gen_class.ProgramLock",
-        mock.Mock(
-            side_effect=mysql_db_admin.gen_class.SingleInstanceException))
-    @mock.patch("mysql_db_admin.run_program", mock.Mock(return_value=True))
-    @mock.patch("mysql_db_admin.gen_libs.help_func",
-                mock.Mock(return_value=False))
-    @mock.patch("mysql_db_admin.arg_parser")
-    def test_programlock_false(self, mock_arg):
-
-        """Function:  test_programlock_false
-
-        Description:  Test with ProgramLock returns False.
-
-        Arguments:
-
-        """
-
-        mock_arg.arg_parse2.return_value = self.args_array
-        mock_arg.arg_require.return_value = False
-        mock_arg.arg_xor_dict.return_value = True
-        mock_arg.arg_cond_req.return_value = True
-        mock_arg.arg_dir_chk_crt.return_value = False
-        mock_arg.arg_file_chk.return_value = False
-
-        with gen_libs.no_std_out():
-            self.assertFalse(mysql_db_admin.main())
-
-    @mock.patch("mysql_db_admin.gen_class.ProgramLock",
-                mock.Mock(side_effect=None))
-    @mock.patch("mysql_db_admin.run_program", mock.Mock(return_value=True))
-    @mock.patch("mysql_db_admin.gen_libs.help_func",
-                mock.Mock(return_value=False))
-    @mock.patch("mysql_db_admin.arg_parser")
-    def test_programlock_true(self, mock_arg):
-
-        """Function:  test_programlock_true
-
-        Description:  Test with ProgramLock returns True.
-
-        Arguments:
-
-        """
-
-        mock_arg.arg_parse2.return_value = self.args_array
-        mock_arg.arg_require.return_value = False
-        mock_arg.arg_xor_dict.return_value = True
-        mock_arg.arg_cond_req.return_value = True
-        mock_arg.arg_dir_chk_crt.return_value = False
-        mock_arg.arg_file_chk.return_value = False
-
-        self.assertFalse(mysql_db_admin.main())
+        self.args_array2 = {"-c": "CfgFile", "-d": "CfgDir", "-y": "Flavor"}
+        self.proglock = ProgramLock(["cmdline"], "FlavorID")
 
     @mock.patch("mysql_db_admin.gen_libs.help_func")
     @mock.patch("mysql_db_admin.arg_parser.arg_parse2")
@@ -366,13 +344,12 @@ class UnitTest(unittest.TestCase):
 
         self.assertFalse(mysql_db_admin.main())
 
-    @mock.patch("mysql_db_admin.gen_class.ProgramLock",
-                mock.Mock(side_effect=None))
     @mock.patch("mysql_db_admin.run_program", mock.Mock(return_value=True))
     @mock.patch("mysql_db_admin.gen_libs.help_func",
                 mock.Mock(return_value=False))
+    @mock.patch("mysql_db_admin.gen_class.ProgramLock")
     @mock.patch("mysql_db_admin.arg_parser")
-    def test_arg_file_false(self, mock_arg):
+    def test_arg_file_false(self, mock_arg, mock_lock):
 
         """Function:  test_arg_file_false
 
@@ -388,6 +365,110 @@ class UnitTest(unittest.TestCase):
         mock_arg.arg_cond_req.return_value = True
         mock_arg.arg_dir_chk_crt.return_value = False
         mock_arg.arg_file_chk.return_value = False
+        mock_lock.return_value = self.proglock
+
+        self.assertFalse(mysql_db_admin.main())
+
+    @mock.patch("mysql_db_admin.run_program", mock.Mock(return_value=True))
+    @mock.patch("mysql_db_admin.gen_libs.help_func",
+                mock.Mock(return_value=False))
+    @mock.patch("mysql_db_admin.gen_class.ProgramLock")
+    @mock.patch("mysql_db_admin.arg_parser")
+    def test_run_program(self, mock_arg, mock_lock):
+
+        """Function:  test_run_program
+
+        Description:  Test run_program function.
+
+        Arguments:
+
+        """
+
+        mock_arg.arg_parse2.return_value = self.args_array
+        mock_arg.arg_require.return_value = False
+        mock_arg.arg_xor_dict.return_value = True
+        mock_arg.arg_cond_req.return_value = True
+        mock_arg.arg_dir_chk_crt.return_value = False
+        mock_arg.arg_file_chk.return_value = False
+        mock_lock.return_value = self.proglock
+
+        self.assertFalse(mysql_db_admin.main())
+
+    @mock.patch("mysql_db_admin.run_program", mock.Mock(return_value=True))
+    @mock.patch("mysql_db_admin.gen_libs.help_func",
+                mock.Mock(return_value=False))
+    @mock.patch("mysql_db_admin.gen_class.ProgramLock")
+    @mock.patch("mysql_db_admin.arg_parser")
+    def test_programlock_true(self, mock_arg, mock_lock):
+
+        """Function:  test_programlock_true
+
+        Description:  Test with ProgramLock returns True.
+
+        Arguments:
+
+        """
+
+        mock_arg.arg_parse2.return_value = self.args_array
+        mock_arg.arg_require.return_value = False
+        mock_arg.arg_xor_dict.return_value = True
+        mock_arg.arg_cond_req.return_value = True
+        mock_arg.arg_dir_chk_crt.return_value = False
+        mock_arg.arg_file_chk.return_value = False
+        mock_lock.return_value = self.proglock
+
+        self.assertFalse(mysql_db_admin.main())
+
+    @mock.patch("mysql_db_admin.run_program", mock.Mock(return_value=True))
+    @mock.patch("mysql_db_admin.gen_libs.help_func",
+                mock.Mock(return_value=False))
+    @mock.patch("mysql_db_admin.gen_class.ProgramLock")
+    @mock.patch("mysql_db_admin.arg_parser")
+    def test_programlock_false(self, mock_arg, mock_lock):
+
+        """Function:  test_programlock_false
+
+        Description:  Test with ProgramLock returns False.
+
+        Arguments:
+
+        """
+
+        mock_arg.arg_parse2.return_value = self.args_array
+        mock_arg.arg_require.return_value = False
+        mock_arg.arg_xor_dict.return_value = True
+        mock_arg.arg_cond_req.return_value = True
+        mock_arg.arg_dir_chk_crt.return_value = False
+        mock_arg.arg_file_chk.return_value = False
+        mock_lock.side_effect = \
+            mysql_db_admin.gen_class.SingleInstanceException
+
+
+        with gen_libs.no_std_out():
+            self.assertFalse(mysql_db_admin.main())
+
+    @mock.patch("mysql_db_admin.run_program", mock.Mock(return_value=True))
+    @mock.patch("mysql_db_admin.gen_libs.help_func",
+                mock.Mock(return_value=False))
+    @mock.patch("mysql_db_admin.gen_class.ProgramLock")
+    @mock.patch("mysql_db_admin.arg_parser")
+    def test_programlock_id(self, mock_arg, mock_lock):
+
+        """Function:  test_programlock_id
+
+        Description:  Test ProgramLock with flavor ID.
+
+        Arguments:
+
+        """
+
+        mock_arg.arg_parse2.return_value = self.args_array2
+        mock_arg.arg_require.return_value = False
+        mock_arg.arg_xor_dict.return_value = True
+        mock_arg.arg_cond_req.return_value = True
+        mock_arg.arg_dir_chk_crt.return_value = False
+        mock_arg.arg_file_chk.return_value = False
+        mock_lock.return_value = self.proglock
 
         self.assertFalse(mysql_db_admin.main())
 
