ea5e5dc28583cf1f8b554e56035c629610f849c8
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-05-20 09:02:03 +0100

    Update mysql_db_admin.py v3.1.1

--- mysql_db_admin.py
+++ mysql_db_admin.py
@@ -242,8 +242,8 @@ analyze instead":
                 continue
 
             else:
-                print("DB: {0:20} Table: {1:35}  Optimized:\t".format(dbs,
-                    tbl), end="")
+                print("DB: {0:20} Table: {1:35}  Optimized:\t".format(
+                    dbs, tbl), end="")
                 gen_libs.prt_msg(item["Msg_type"], item["Msg_text"])
 
 
