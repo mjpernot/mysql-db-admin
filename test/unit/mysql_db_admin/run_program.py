a11f9c9a7ede906e3e1752abd6f5135ec431d34f
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2021-03-02 10:41:05 +0000

    Updated test/unit/mysql_db_admin/run_program.py v3.1.3

--- test/unit/mysql_db_admin/run_program.py
+++ test/unit/mysql_db_admin/run_program.py
@@ -29,6 +29,7 @@ import mock
 # Local
 sys.path.append(os.getcwd())
 import mysql_db_admin
+import lib.gen_libs as gen_libs
 import version
 
 __version__ = version.__version__
@@ -80,9 +81,10 @@ class Server(object):
 
         """
 
-        pass
+        self.name = "Server_Name"
+        self.conn_msg = None
 
-    def connect(self):
+    def connect(self, silent=False):
 
         """Method:  connect
 
@@ -92,7 +94,12 @@ class Server(object):
 
         """
 
-        return True
+        status = True
+
+        if silent:
+            status = True
+
+        return status
 
 
 class UnitTest(unittest.TestCase):
@@ -103,6 +110,8 @@ class UnitTest(unittest.TestCase):
 
     Methods:
         setUp -> Initialize testing environment.
+        test_connect_failure -> Test with failed connection.
+        test_connect_success -> Test with successful connection.
         test_email -> Test with email option.
         test_mongo -> Test with mongo option.
         test_run_program -> Test run_program function.
@@ -125,6 +134,45 @@ class UnitTest(unittest.TestCase):
         self.args_array2 = {"-m": True, "-d": True, "-c": True, "-C": True,
                             "-e": "ToEmail", "-s": "SubjectLine"}
         self.args_array3 = {"-d": True, "-c": True, "-C": True}
+
+    @mock.patch("mysql_db_admin.mysql_libs.disconnect")
+    @mock.patch("mysql_db_admin.mysql_libs.create_instance")
+    def test_connect_failure(self, mock_inst, mock_disconn):
+
+        """Function:  test_connect_failure
+
+        Description:  Test with failed connection.
+
+        Arguments:
+
+        """
+
+        self.server.conn_msg = "Error connection message"
+
+        mock_inst.return_value = self.server
+        mock_disconn.return_value = True
+
+        with gen_libs.no_std_out():
+            self.assertFalse(mysql_db_admin.run_program(self.args_array3,
+                                                        self.func_dict))
+
+    @mock.patch("mysql_db_admin.mysql_libs.disconnect")
+    @mock.patch("mysql_db_admin.mysql_libs.create_instance")
+    def test_connect_success(self, mock_inst, mock_disconn):
+
+        """Function:  test_connect_success
+
+        Description:  Test with successful connection.
+
+        Arguments:
+
+        """
+
+        mock_inst.return_value = self.server
+        mock_disconn.return_value = True
+
+        self.assertFalse(mysql_db_admin.run_program(self.args_array3,
+                                                    self.func_dict))
 
     @mock.patch("mysql_db_admin.mysql_libs.disconnect")
     @mock.patch("mysql_db_admin.gen_libs.load_module")
