3005f8a78c1acbf55f10ce3d163dc9e8475c88e0
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2021-08-18 16:55:27 +0100

    Updated test/unit/mysql_db_admin/checksum.py v3.2.0

--- test/unit/mysql_db_admin/checksum.py
+++ test/unit/mysql_db_admin/checksum.py
@@ -41,7 +41,7 @@ class Server(object):
     Description:  Class stub holder for mysql_class.Server class.
 
     Methods:
-        __init__ -> Class initialization.
+        __init__
 
     """
 
@@ -65,8 +65,8 @@ class UnitTest(unittest.TestCase):
     Description:  Class which is a representation of a unit testing.
 
     Methods:
-        setUp -> Initialize testing environment.
-        test_checksum -> Test checksum function.
+        setUp
+        test_checksum
 
     """
 
